8f6a787602402c694f4e4d36599ccb79c3cda8a2
Anthony <anthonyfdez23@gmail.com>
2022-09-10 07:22:26 -0400

    Fixed

--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
