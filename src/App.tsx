1d990ed31f894652010b1001bed7d1258885e1ec
Anthony <anthonyfdez23@gmail.com>
2022-09-10 04:59:52 -0400

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,19 @@ import "./App.css";
 import { Link, Route, Routes } from "react-router-dom";
 
 // Components
-import AlgorithmVisualizer from "./components/graph/graph";
 import Header from "./components/header/header";
 import PathFinder from "./components/pathfinder/pathfinder";
-import HomePage from "./pages/home/home";
 import Test from "./pages/test/test";
+import Visualizer from "./pages/visualizer/visualizer";
+
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="page-container">
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route
-            path="/algorithm-visualizer"
-            element={<AlgorithmVisualizer />}
-          />
+          {/* <Route path="/" element={<HomePage />} /> */}
+          <Route path="/algorithm-visualizer" element={<Visualizer />} />
           <Route path="/pathfinder" element={<PathFinder />} />
           <Route path="/test" element={<Test />} />
         </Routes>
