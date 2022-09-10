c204a4329017956168b83a1fa72607e0c9bc8fdd
AlejandroV01 <alexvera0109@gmail.com>
2022-09-10 03:11:05 -0700

    Added nav and Routers

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -10,10 +10,10 @@ const Header = (): JSX.Element => {
           <a href="/">
             <h2>Home</h2>
           </a>
-          <a href="/">
+          <a href="/algorithm-visualizer">
             <h2>Algorithm Visualizer</h2>
           </a>
-          <a href="/">
+          <a href="/pathfinder">
             <h2>Pathfinder</h2>
           </a>
         </div>
---
+++ src/components/pathfinder/pathfinder.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const pathfinder = () => {
+  return (
+    <div>
+      <h1>Pathfinder Page</h1>
+    </div>
+  );
+};
+
+export default pathfinder;
