6325d46aa8afdbd6fb2e0f05c60f40cb997b0888
Anthony <anthonyfdez23@gmail.com>
2022-09-09 23:45:50 -0400

    Added home page

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Graph from "../../components/graph/graph";
+
+const HomePage = (): JSX.Element => {
+  return (
+    <div>
+      <Graph />
+    </div>
+  );
+};
+
+export default HomePage;
