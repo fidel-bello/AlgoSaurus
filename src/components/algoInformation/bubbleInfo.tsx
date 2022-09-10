f650928e6c4049a6fa10a2027721fb237651a240
AlejandroV01 <alexvera0109@gmail.com>
2022-09-10 02:47:46 -0700

    Added info files

---
+++ src/components/algoInformation/bubbleInfo.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const bubbleInfo = () => {
+  return (
+    <div>
+      <p>
+        Bubble sort is a basic algorithm for arranging a string of numbers or
+        other elements in the correct order. The method works by examining each
+        set of adjacent elements in the string, from left to right, switching
+        their positions if they are out of order. The algorithm then repeats
+        this process until it can run through the entire string and find no two
+        elements that need to be swapped.
+      </p>
+      <p>
+        Computer programmers use bubble sort to arrange a sequence of numbers in
+        the correct order. Because it is the simplest type of sorting algorithm,
+        bubble sort does not get used much in real-world computer science.
+      </p>
+      <p>
+        Because it has to repeatedly cycle through the entire set of elements,
+        comparing only two adjacent items at a time, bubble sort is not optimal
+        for more massive datasets. But it can work well when sorting only a
+        small number of elements.
+      </p>
+    </div>
+  );
+};
+
+export default bubbleInfo;
---
+++ src/components/algoInformation/insertionInfo.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const insertionInfo = () => {
+  return (
+    <div>
+      <p>
+        Insertion sort is a simple sorting algorithm that works similar to the
+        way you sort playing cards in your hands. The array is virtually split
+        into a sorted and an unsorted part. Values from the unsorted part are
+        picked and placed at the correct position in the sorted part.
+      </p>
+      <p>
+        This algorithm is one of the simplest algorithm with simple
+        implementation. Basically, insertion sort is efficient for small data
+        values. Insertion sort is adaptive in nature, i.e. it is appropriate for
+        data sets which are already partially sorted.
+      </p>
+    </div>
+  );
+};
+
+export default insertionInfo;
