5f8f006495601840051d34877f849a8b5c40f07b
Anthony <anthonyfdez23@gmail.com>
2022-09-10 20:13:19 -0400

    Refined the animation

--- src/pages/visualizer/visualizer.tsx
+++ src/pages/visualizer/visualizer.tsx
@@ -119,7 +119,7 @@ const Visualizer = (): JSX.Element | null => {
   };
 
   const container = {
-    hidden: { opacity: 0, y: -200 },
+    hidden: { opacity: 0, y: -100 },
     show: {
       opacity: 1,
       y: 0,
