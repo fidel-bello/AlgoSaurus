d27912e5031f05c1055fe39e513aaa02acb89b4f
Anthony <anthonyfdez23@gmail.com>
2022-09-10 23:59:18 -0400

    removed unused code

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
