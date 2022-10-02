ff428e8c72a7ae26efd85fe13c0d94019fc21513
Anthony <anthonyfdez23@gmail.com>
2022-10-02 00:02:18 -0400

    Add particles
    - THis is still very buggy

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,7 +3,26 @@ import { Link } from "react-router-dom";
 import styles from "./home.module.css";
 import { motion } from "framer-motion";
 
+// Particles background
+import { tsParticles } from "tsparticles-engine";
+import { useCallback } from "react";
+import Particles from "react-particles";
+import type { Container, Engine } from "tsparticles-engine";
+import { loadFull } from "tsparticles";
+import { loadFirePreset } from "tsparticles-preset-fire";
+
 const home = () => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFirePreset(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      await console.log(container);
+    },
+    []
+  );
+
   const container = {
     hidden: { opacity: 0, y: 0 },
     show: {
@@ -21,27 +40,35 @@ const home = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <motion.div
-        initial="hidden"
-        animate="show"
-        variants={container}
-        className={styles.hero}
-      >
-        <motion.h1 variants={item} className={styles.heroHeader}>
-          Algorithm Visualizer
-        </motion.h1>
-        <motion.p variants={item} className={styles.heroPara}>
-          A tool for explaining concept of sorting algorithms in computing.
-        </motion.p>
-        <br></br>
-        <motion.div variants={item}>
-          <Link to="/algorithm-visualizer">
-            <button className={styles.heroButton}>Learn More</button>
-          </Link>
+    <>
+      <Particles
+        options={{ preset: "fire" }}
+        init={particlesInit}
+        loaded={particlesLoaded}
+        className={styles.particles_background}
+      />
+      <div className={styles.container}>
+        <motion.div
+          initial="hidden"
+          animate="show"
+          variants={container}
+          className={styles.hero}
+        >
+          <motion.h1 variants={item} className={styles.heroHeader}>
+            Algorithm Visualizer
+          </motion.h1>
+          <motion.p variants={item} className={styles.heroPara}>
+            A tool for explaining concept of sorting algorithms in computing.
+          </motion.p>
+          <br></br>
+          <motion.div variants={item}>
+            <Link to="/algorithm-visualizer">
+              <button className={styles.heroButton}>Learn More</button>
+            </Link>
+          </motion.div>
         </motion.div>
-      </motion.div>
-    </div>
+      </div>
+    </>
   );
 };
 
