7948bb4957727725245be8f39febd6ffcbad2d21
Fidel <fidelbello1997@gmail.com>
2023-07-29 14:20:21 -0400

    created: added state of grid

--- src/pages/pathFinding/pathFinding.tsx
+++ src/pages/pathFinding/pathFinding.tsx
@@ -1,10 +1,28 @@
 import React from "react";
-import PathFindingGraph from "../../components/graphs/pathFindingGraph";
+import PathFindingGraph, {
+  InitialGraphState,
+} from "../../components/graphs/pathFindingGraph";
 
+const initialState: InitialGraphState = {
+  grid: [],
+  startNode: 5,
+  endNode: 5,
+  startNodeCol: 5,
+  endNodeCol: 15,
+  rowCount: 25,
+  columnCount: 35,
+  isMousePressed: false,
+  isRunning: false,
+  isStartNode: false,
+  isFinishNode: false,
+  isWallNode: false,
+  currRow: 0,
+  currCol: 0,
+  isDesktopView: true,
+};
 
-const PathFinding: React.FC = () => { 
-
-    return <PathFindingGraph></PathFindingGraph>
-}
+const PathFinding: React.FC = () => {
+  return <PathFindingGraph {...initialState } />;
+};
 
 export default PathFinding;
