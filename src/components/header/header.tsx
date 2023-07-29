d5ca98bddfb9513dc3a028fafeb488da0bbbbb6c
Fidel <fidelbello1997@gmail.com>
2023-07-29 09:37:37 -0400

    added: added route check for display pathfinder

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import dino from "../dino.gif";
 import styles from "./header.module.css";
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -8,6 +7,18 @@ import MobileMenu from "./mobileMenu/mobileMenu";
 
 const Header = (): JSX.Element => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [ isHidden, setIsHidden ] = useState<boolean>(false);
+  const location = useLocation();
+
+  const handleHidden = (state: boolean): void => {
+    setIsHidden(state)
+  }
+
+
+  useEffect(() => {
+  if (location.pathname === "/algorithm-visualizer") handleHidden(false);
+  else handleHidden(true);
+  }, [location.pathname]);
 
   return (
     <div className={styles.container}>
@@ -24,7 +35,10 @@ const Header = (): JSX.Element => {
             <h1>Saurus</h1>
           </div>
         </Link>
-        <div className={styles.menuItemDiv}>
+        <div className={styles.menuItems}>
+          <Link to="/path-finding" className={styles.menuItems} style={ isHidden ? { display: "none"}: { display: "block"} }>
+            <span className={styles.menuHeader}>Path Finding</span>
+          </Link>
           <Link to="/algorithm-visualizer" className={styles.menuItems}>
             <span className={styles.menuHeader}>Visualizer</span>
           </Link>
