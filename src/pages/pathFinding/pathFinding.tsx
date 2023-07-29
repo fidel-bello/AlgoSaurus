1ad8595a87095c7b4f0d8e87b0a957422d19c0fc
Fidel <fidelbello1997@gmail.com>
2023-07-29 09:38:28 -0400

    return pathFinding component

---
+++ src/pages/pathFinding/pathFinding.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PathFindingGraph from "../../components/graphs/pathFindingGraph";
+
+
+const PathFinding: React.FC = () => { 
+
+    return <PathFindingGraph></PathFindingGraph>
+}
+
+export default PathFinding;
