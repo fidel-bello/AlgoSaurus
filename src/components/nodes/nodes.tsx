b9869870b6d0b8db931955424735c494f2e2311b
Fidel <fidelbello1997@gmail.com>
2023-10-30 01:39:39 -0400

    added events and props to the node component

--- src/components/nodes/nodes.tsx
+++ src/components/nodes/nodes.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import styles from "./nodes.module.css";
 import { Node } from "../../helpers/interfaces/algoInterface";
 
-const Nodes = (props: Node) => {
+
+const Nodes =({ row, col, isFinish, isWallNode, isStart, handleMouseDown, handleMouseUp, handleOnMouseEnter }: Node) => {
+ 
   return (
     <td
-      id={`node-${props.row}-${props.col}`}
-      className={`${styles.node} ${props.isFinish ? styles.node_finish : 
-        props.isStart ? styles.node_start : props.isWallNode ? styles.node_wall : ""}`}
+      id={`node-${row}-${col}`}
+      className={`${styles.node} ${isFinish ? styles.node_finish :
+        isStart ? styles.node_start : isWallNode ? styles.node_wall : ""}`}
+      onMouseDown={() => handleMouseDown && handleMouseDown(row, col)}
+      onMouseUp={() => handleMouseUp && handleMouseUp(row, col)}
+      onMouseEnter={() => handleOnMouseEnter && handleOnMouseEnter(row, col)}
     ></td>
   );
 };
